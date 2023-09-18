6737931500da7766eac354a4351612b097e729d6
Takemoto <victor_takemoto@outlook.com>
2023-09-18 10:37:32 -0300

    FinalizadoChamadaAPI

--- onmViz.Stream/FetchAPI.cs
+++ onmViz.Stream/FetchAPI.cs
@@ -1,5 +1,8 @@
-﻿using onmViz.DAL.Model;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using onmViz.DAL.Model;
 using onmViz.DAL.Model.Entity;
+using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -7,8 +10,10 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Nodes;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static Emgu.CV.VideoCapture;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace onmViz.Stream
@@ -23,12 +28,6 @@ namespace onmViz.Stream
         {
             InitComboBox();
         }
-
-        public void EndPointAPI()
-        {
-            //var httpFetch = new 
-        }
-
         public void InitComboBox()
         {
             try
@@ -57,26 +56,57 @@ namespace onmViz.Stream
             this.Close();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private async void button1_Click(object sender, EventArgs e)
         {
-            if (textBox1 == null)
+            if (string.IsNullOrEmpty(textBox1.Text))
             {
-                MessageBox.Show($"Digite palca do veiculo");
+                MessageBox.Show($"Digite placa do veiculo");
                 return;
             }
             if (comboBox1.SelectedIndex != -1)
             {
                 PBox pBox;
-                using(var db = new onmVizDBContext())
+                using (var db = new onmVizDBContext())
                 {
                     pBox = db.PictureBoxes.Where(d => d.PictureBoxName == comboBox1.Text).FirstOrDefault();
                 }
-                int IDRecurso = pBox.IDRecurso;
+                string IDRecurso = pBox.IDRecurso.ToString();
                 string placaAuto = textBox1.Text;
-                MessageBox.Show($"Parametros: {IDRecurso}\n{placaAuto}");
+
+                await CallAPI(IDRecurso, placaAuto);
                 return;
             }
             MessageBox.Show($"Combobox vazia");
         }
+
+        public async Task CallAPI(string id, string placa)
+        {
+            try
+            {
+                //TODO: Parametrizar URL via json
+                var options = new RestClientOptions("http://10.10.4.58:8082")
+                {
+                    MaxTimeout = -1,
+                };
+                var client = new RestClient(options);
+                var request = new RestRequest($"/api/Operacao/ProcessarManual?IDRecurso={id}&placa={placa}", Method.Post);
+
+                RestResponse response = await client.ExecuteAsync(request);
+                //MessageBox.Show($"Teste: {response.Content}");
+                if (response.IsSuccessful)
+                {
+                    ResponseAPI resultado = JsonConvert.DeserializeObject<ResponseAPI>(response.Content);
+                    MessageBox.Show($"Tipo: {resultado.tipo}\n" +
+                                    $"Informação: {resultado.informacao}\n" +
+                                    $"Mensagem: {resultado.mensagem}");
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Deu Erro {ex.Message}");
+            }
+        }
+
     }
 }
---
+++ onmViz.Stream/ResponseAPI.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace onmViz.Stream
+{
+    public class ResponseAPI
+    {
+        public string tipo { get; set; }
+        public string informacao { get; set; }
+        public string mensagem { get; set; }
+    }
+}
